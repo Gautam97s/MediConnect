47dc50d25680720375680f48c554119ab191a894
Gautam Sharma <gautam.mahi10@gmail.com>
2026-03-23 00:34:56 +0530

    chore: Remove .next cache from git tracking and update .gitignore

--- frontend/.next/dev/server/pages/doctor-dashboard.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/doctor-dashboard.js")
-R.c("server/chunks/ssr/node_modules_058sknj._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0xu.7zd._.js")
-R.c("server/chunks/ssr/node_modules_047xm.d._.js")
-R.c("server/chunks/ssr/[externals]__0.o~_~l._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0yxga~9._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/doctor-dashboard.jsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.jsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/doctor-dashboard.jsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.jsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
--- frontend/.next/dev/static/chunks/src_pages_pharmacy_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js","static/chunks/node_modules_next_dist_shared_lib_0vy467f._.js","static/chunks/node_modules_next_dist_client_00_u_l7._.js","static/chunks/node_modules_next_dist_0k8zr~w._.js","static/chunks/node_modules_next_0k13yvq._.js","static/chunks/node_modules_react-dom_0bruynb._.js","static/chunks/node_modules_0xee5hx._.js","static/chunks/[root-of-the-server]__03jq9cq._.js"],
-    source: "entry"
-});
